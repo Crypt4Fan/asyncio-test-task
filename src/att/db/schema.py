bba153fd5b7d523baeb63e130b813352409553db
user <user@user.user>
2019-11-05 18:32:14 +0800

    Add db tables for group

--- src/att/db/schema.py
+++ src/att/db/schema.py
@@ -27,3 +27,21 @@ t_user = sa.Table(
     sa.CheckConstraint('lower(login) = login', name='login_is_lowercase'),
     sa.CheckConstraint('length(password) > 0', name='password_len_gt_0'),
 )
+
+t_group = sa.Table(
+    'group', metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('name', sa.String, nullable=False),
+    sa.CheckConstraint('length(name) > 0', name='group_name_len_gt_0'),
+    sa.CheckConstraint('lower(name) = name', name='group_name_is_lowercase'),
+    sa.UniqueConstraint('name')
+)
+
+t_user_group = sa.Table(
+    'user_group', metadata,
+    sa.Column('user_id', pg.UUID(as_uuid=True),
+              sa.ForeignKey('user.id', ondelete='CASCADE'), nullable=False),
+    sa.Column('group_id', sa.Integer,
+              sa.ForeignKey('group.id', ondelete='CASCADE'), nullable=False),
+    sa.UniqueConstraint('user_id', 'group_id')
+)
